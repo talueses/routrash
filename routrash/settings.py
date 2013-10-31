27bc0abaa02319ade42e7492c5ae1ab5f12cbb30
Jin Kadaba <talueses@gmail.com>
2013-10-31 17:02:15 -0430

    Modifica la data

--- routrash/settings.py
+++ routrash/settings.py
@@ -3,26 +3,6 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 import os
 RUTA_PROYECTO = os.path.dirname(os.path.realpath(__file__))
 
